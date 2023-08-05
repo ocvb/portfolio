7f8396cc8b1c47684ed8e8612e0ec5aabf6abfc5
ocvb <electrokai1@gmail.com>
2023-08-06 00:24:48 +0800

    updateeeeee

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,12 +1,12 @@
 $(function () {
-   $(".transition-top-down").delay(200).each(function (i) {
-      $(this).delay(400 / 1.5 * i).queue(function () {
+   $(".transition-top-down").each(function (i) {
+      $(this).delay(450 / 1.5 * i).queue(function () {
          $(this).css({ "opacity": 1, "transform": "translateY(0)" });
       })
    });
 
-   $(".transition-left-right").delay(200).each(function (i) {
-      $(this).delay(400 / 1.5 * i).queue(function () {
+   $(".transition-left-right").each(function (i) {
+      $(this).delay(450 / 1.5 * i).queue(function () {
          $(this).css({ "opacity": 1, "transform": "translateX(0)" });
       })
    });
